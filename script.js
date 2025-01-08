01b99f12967a8a1f59b323da90505a9b860b58f7
kilgers <73341227+kilgers@users.noreply.github.com>
2025-01-07 19:27:10 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,104 @@
+        const selectElement = document.getElementById("playerName");
+        selectElement.addEventListener("change", async function(){
+            const selectedValue = this.value;
+            const url = "https://api.letsplaywithballs.com/?name=" + selectedValue + "&get_active_years=True"
+            try{
+                const response = await
+                fetch(url, {mode: 'cors'});
+                if(!response.ok){
+			console.log(response.status);
+			console.log(response.url);
+                }
+                const json = await
+                response.json();
+                const selectElement = document.getElementById("year");
+                selectElement.innerHTML = "";
+                console.log(json)
+                let seasonArray = JSON.stringify(json);
+                seasonArray = JSON.parse(seasonArray);
+                for(let i = 0; i < seasonArray.length; i++){
+                    const option = document.createElement("option");
+                    option.value = seasonArray[i];
+                    option.text = seasonArray[i];
+                    selectElement.add(option);
+                }
+                console.log(json);
+            } catch (error) {
+            console.error(error.message)}
+        })
+        const selectElement2 = document.getElementById("player2Name");
+        selectElement2.addEventListener("change", async function(){
+            const selectedValue = this.value;
+            const url = "https://api.letsplaywithballs.com/?name=" + selectedValue + "&get_active_years=True"
+            try{
+                const response = await
+                fetch(url, {mode: 'cors'});
+                if(!response.ok){
+			console.log(response.status);
+			console.log(response.url);
+                }
+                const json = await
+                response.json();
+                const selectElement = document.getElementById("year2");
+                selectElement.innerHTML = "";
+                console.log(json)
+                let seasonArray = JSON.stringify(json);
+                seasonArray = JSON.parse(seasonArray);
+                for(let i = 0; i < seasonArray.length; i++){
+                    const option = document.createElement("option");
+                    option.value = seasonArray[i];
+                    option.text = seasonArray[i];
+                    selectElement.add(option);
+                }
+                console.log(json);
+            } catch (error) {
+            console.error(error.message)}
+        })
+        const formElement = document.getElementById("chartForm")
+        formElement.addEventListener("submit", async function(event){
+		event.preventDefault();
+		const formdata = new FormData(formElement);
+		let playername = formdata.get("playername");
+		let year = formdata.get("year");
+		let player2name = formdata.get("playername");
+		console.log(player2name)
+		let year2 = formdata.get("year2");
+		
+		const url = "https://api.letsplaywithballs.com/?name=" + playername + "&year=" + year;
+            try{
+                const response = await
+                fetch(url, {mode: 'cors'});
+                if(!response.ok){
+                    throw new Error("Response status: ${response.status}");
+                }
+                const divText = await
+                response.text();
+                const selectDiv = document.getElementById("chartDiv");
+                selectDiv.innerHTML = divText;
+            } catch (error) {
+            console.error(error.message)};
+			
+		if (typeof player2name !== "undefined"){
+		const url = "https://api.letsplaywithballs.com/?name=" + player2name + "&year=" + year2;
+            try{
+                const response = await
+                fetch(url, {mode: 'cors'});
+                if(!response.ok){
+                    throw new Error("Response status: ${response.status}");
+                }
+                const divText = await
+                response.text();
+                const selectDiv = document.getElementById("chartDiv");
+                selectDiv.innerHTML += divText;
+		    console.log(divText);
+			} catch (error) {
+            console.error(error.message)}
+        }
+		})
+		function showSecondChart(){
+		let hiddenElements = document.getElementsByClassName("player2");
+		console.log(hiddenElements);
+		for(let i = 0; i < hiddenElements.length; i++){
+		hiddenElements[i].style.display = "block";
+		}
+		}
